516c13a062a828e38b50a9f6d0f5504a79b13f61
AkshayJana646 <akshayjana646@gmail.com>
2025-10-29 13:40:18 -0400

    Initial commit of OTV project

---
+++ config.py
@@ -0,0 +1,37 @@
+#Put hardware specific pins and whatever constants here
+
+#Motor driver pins
+
+
+#Servo pin for dispensing atoms
+
+#Voltage sensor ADC pin
+
+
+#LED color sensor pins
+
+
+#Team and Vision System Setup
+
+ARUCO_ID = 67
+ROOM = 1120
+TEAM_NAME = "Maryland Dairy"
+MISSION_TYPE = "DATA" # Change later
+
+#Navigation and PID Constants
+
+NAV_TOLERANCE = 0.15 # Tune (meters)
+TURN_TOLERANCE = 6.0 # Tune (degrees)
+
+kP = 0.0 # Tune
+kI = 0.0 # Tune
+kD = 0.0 # Tune
+
+
+
+#Min and Max Speeds (Scale of 0-100)
+
+MAX_SPEED = 100
+MIN_SPEED = 30 # Tune
+
+
